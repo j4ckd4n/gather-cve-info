af8a2de24884ea09f9589d7cb3d92406fd2c75c6
jackdan <arnas.jonas.a@gmail.com>
2022-11-15 17:34:10 -0600

    Add files via upload

---
+++ src/utils.ts
@@ -0,0 +1,35 @@
+export class MarkdownAdder {
+    string_buffer: Array<String>;
+    
+    constructor(){
+        this.string_buffer = new Array();
+    }
+    
+    addHeader1(text: string){
+        this.string_buffer.push(`# ${text}\n`);
+    }
+
+    addHeader2(text: string){
+        this.string_buffer.push(`## ${text}\n`);
+    }
+
+    addContent(text: string){
+        this.string_buffer.push(`${text}\n`);
+    }
+
+    addListItem(text: string){
+        this.string_buffer.push(`- ${text}\n`);
+    }
+
+    addInfoItem(item_name: string, text: string){
+        this.string_buffer.push(`${item_name}:: ${text}\n`);
+    }
+
+    addSplit(){
+        this.string_buffer.push('\n---\n')
+    }
+
+    toString(){
+        return this.string_buffer.filter(s => s).join('');
+    }
+}
